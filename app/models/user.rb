8bcce6cdfebab3c3ba60136724b1dc6648ce63a0
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-01-07 10:19:18 +0000

    [Add]follow_function #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,26 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :follower, class_name: "Reletionship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Reletionship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 
   attachment :profile_image
   attachment :home_image
 
   validates :name, presence: true, length: { maximum: 30, allow_blank: true }
+  
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_user.include?(user)
+  end
 
 end
