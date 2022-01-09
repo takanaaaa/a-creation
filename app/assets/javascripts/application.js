641490c26fe088acb0e0c5743d6db4cbf0afa638
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-01-09 09:24:29 +0000

    [layouyts]nav-bar #36

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,20 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+/*global $*/
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+}
