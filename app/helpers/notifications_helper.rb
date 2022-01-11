d1b7d152c86da2986cf742671f5223d4677c38f3
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-01-11 05:20:45 +0000

    [layouts]notice #44

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -10,10 +10,10 @@ module NotificationsHelper
         tag.a(@visiter.name)+"があなたをフォローしました"
       when "comment" then
         @comment = PostComment.find_by(id: @visiter_comment)&.content
-        tag.a(@visiter.name)+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id))+"にコメントしました"
-      end
+        tag.a(@visiter.name)+"が"+tag.a('▶︎あなたの投稿', href:post_path(notification.post_id))+"にコメントしました"
+    end
   end
-  
+
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
