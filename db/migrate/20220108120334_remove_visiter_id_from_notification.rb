de3212550adbbc0b201c7c87aa5b63f6f9b0c56b
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-01-18 05:06:59 +0000

    [Fix]visiter_id

--- db/migrate/20220108120334_remove_visiter_id_from_notification.rb
+++ db/migrate/20220108120334_remove_visiter_id_from_notification.rb
@@ -1,5 +1,7 @@
 class RemoveVisiterIdFromNotification < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :notifications, :visiter_id, foreign_key:{to_table: :users}
+    remove_foreign_key :notifications, :users
+    remove_index :notifications, :visiter_id
+    remove_reference :notifications, :visiter
   end
 end
