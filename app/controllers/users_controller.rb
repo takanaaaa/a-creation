cb66b66ce5175ca30f7aaf35245a36c783fce992
takanaaaa <se-ha_1v4@docomo.ne.jp>
2022-02-04 14:04:00 +0000

    [Fix]ensure_correct_user #4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, except: [:index]
+  before_action :ensure_correct_user, only: [:edit]
 
   def show
     @posts = @user.posts.page(params[:page]).per(12)
@@ -24,5 +25,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile, :profile_image, :home_image)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to edit_user_path(current_user)
+    end
   end
 end
